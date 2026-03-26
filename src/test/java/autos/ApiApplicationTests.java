630ddb8d31faee195f2c4540760a60b790ecd4fe
gustavoguearaaa <gustavog090615@gmail.com>
2026-03-26 12:56:51 -0300

    primer commit

---
+++ src/test/java/autos/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
