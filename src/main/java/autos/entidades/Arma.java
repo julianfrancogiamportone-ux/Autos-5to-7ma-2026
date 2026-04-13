ef5ea468a70d01dd3fb2fc8f53c8238d7eacb54b
gustavoguevaraaa <gustavog090615@gmail.com>
2026-04-13 08:40:53 -0300

    Creacion/funcionamiento de entidad arma

--- src/main/java/autos/entidades/Arma.java
+++ src/main/java/autos/entidades/Arma.java
@@ -1,17 +1,37 @@
 package autos.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.*; 
 @Entity
 
 public class Arma {
     @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY))
-    private Long id;  
-    private String nombre (String) 
-    Private int bonificadorDanio (int) 
-    private peso (float)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   
+    private String nombre; 
+    private int bonificadorDanio;
+    private float peso;
+    public Long getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getBonificadorDanio() {
+        return bonificadorDanio;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setBonificadorDanio(int bonificadorDanio) {
+        this.bonificadorDanio = bonificadorDanio;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
 }
-//hola tio
