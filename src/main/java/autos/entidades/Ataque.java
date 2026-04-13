33e607380d4a12693174b00d3a6eea07de0a1c4e
fabriziostag-cmyk <fabriziostag@gmail.com>
2026-04-13 08:44:38 -0300

    creacionentidadataque

--- src/main/java/autos/entidades/Ataque.java
+++ src/main/java/autos/entidades/Ataque.java
@@ -5,10 +5,33 @@ import jakarta.persistence.*;
 
 public class Ataque {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre; 
     private int costoEnergia; 
     private int danioBase;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getCostoEnergia() {
+        return costoEnergia;
+    }
+    public void setCostoEnergia(int costoEnergia) {
+        this.costoEnergia = costoEnergia;
+    }
+    public int getDanioBase() {
+        return danioBase;
+    }
+    public void setDanioBase(int danioBase) {
+        this.danioBase = danioBase;
+    }
 }
-//xd
